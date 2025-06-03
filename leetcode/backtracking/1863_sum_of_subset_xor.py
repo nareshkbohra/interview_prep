ab2161585d47a69c9fc251086e960790334e3ba6
Naresh Bohra <nareshkbohra@gmail.com>
2025-06-03 14:16:13 +0530

    more backtracking

--- leetcode/backtracking/1863_sum_of_subset_xor.py
+++ leetcode/backtracking/1863_sum_of_subset_xor.py
@@ -2,10 +2,16 @@ from typing import List
 
 
 class Solution:
+	def helper(self, nums: List[int], index: int, currXor: int) -> int:
+		if index == len(nums):
+			return currXor
+
+		return self.helper(nums, index + 1, currXor) + self.helper(nums, index + 1, currXor ^ nums[index])
+
 	def subsetXORSum(self, nums: List[int]) -> int:
-		return 0
+		return self.helper(nums, 0, 0)
 
 
 s = Solution()
-res = s.subsetXORSum([1, 3])
+res = s.subsetXORSum([5, 1, 6])
 print(res)
