3f926f44b03f75d8e449cf499d58e18bf012d7d5
Naresh Bohra <nareshkbohra@gmail.com>
2025-06-03 19:31:32 +0530

    back tracking continues

---
+++ leetcode/backtracking/39_combination_sum.py
@@ -0,0 +1,38 @@
+from typing import List
+
+
+class Solution:
+	def helper(self, candidates, index, target):
+		if len(candidates) == index or target <= 0:
+			return []
+
+		result = []
+		first_number = candidates[index]
+		amount = target
+		base_list = []
+		while True:
+			if amount < 0:
+				break
+			if amount == 0:
+				result.append(base_list)
+				break
+			sub_results = self.helper(candidates, index + 1, amount)
+			for sub_result in sub_results:
+				result.append(base_list + sub_result)
+
+			base_list.append(first_number)
+			amount -= first_number
+
+		return result
+
+	def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+		"""
+		First decide frequency of first number then calculate remaining number
+		Append both
+		"""
+		return self.helper(candidates, 0, target)
+
+
+s = Solution()
+res = s.combinationSum([2, 3, 5], 8)
+print(res)
---
+++ leetcode/backtracking/46_permutations.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+	def helper(self, nums: List[int], index: int) -> List[List[int]]:
+		remaining_nums = len(nums) - index
+		if remaining_nums == 0:
+			return []
+		first_num = nums[index]
+		if remaining_nums == 1:
+			return [[first_num]]
+		sub_results = self.helper(nums, index + 1)
+		result = []
+		for sub_result in sub_results:
+			for i in range(len(sub_result) + 1):
+				sub_result_copy = list(sub_result)
+				sub_result_copy.insert(i, first_num)
+				result.append(sub_result_copy)
+		return result
+
+	def permute(self, nums: List[int]) -> List[List[int]]:
+		return self.helper(nums, 0)
+
+
+s = Solution()
+res = s.permute([1, 2, 3])
+print(res)
